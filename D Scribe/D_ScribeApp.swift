f876503c2764ac64288aad02da0de830edb3faf5
vorpus <li3zhang@gmail.com>
2026-04-04 15:05:41 -0400

    Initial Commit

---
+++ D Scribe/D_ScribeApp.swift
@@ -0,0 +1,17 @@
+//
+//  D_ScribeApp.swift
+//  D Scribe
+//
+//  Created by li on 4/4/26.
+//
+
+import SwiftUI
+
+@main
+struct D_ScribeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
